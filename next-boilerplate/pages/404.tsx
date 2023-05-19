bca201c469007d52c14fe108f461c62185e06ef7
yongsungHeo <ysheo94@gmail.com>
2023-05-19 20:53:37 +0900

    404, 500 에러 페이지 추가

---
+++ next-boilerplate/pages/404.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import styles from '@/styles/Error.module.scss';
+
+const Error: NextPage = () => {
+    const router = useRouter();
+
+    return (
+        <div className={styles['error']}>
+            <div>
+                <FaExclamationTriangle />
+                <span className={styles['error__description']}>URL 을 다시 확인해보시겠어요?</span>
+                <p className={styles['error__bug-report']}>
+                    버그 제보 :
+                    <a href="mailto:sample@gmail.com" className={styles['error_bug-report-email']}>
+                        sample@gmail.com
+                    </a>
+                </p>
+            </div>
+            <button className={styles['error__button']} onClick={() => router.push('/')}>홈 화면</button>
+        </div>
+    );
+};
+
+export default Error;
