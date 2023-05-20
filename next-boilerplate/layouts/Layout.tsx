4e1d3e13ba40bf1731297d29ab3c9f8e842af46d
yongsungHeo <ysheo94@gmail.com>
2023-05-20 15:35:12 +0900

    다국어 변경 기능 추가

--- next-boilerplate/layouts/Layout.tsx
+++ next-boilerplate/layouts/Layout.tsx
@@ -1,6 +1,9 @@
+import Lang from '@/container/Lang';
+
 export default function Layout({ children }: any) {
     return (
         <>
+            <Lang />
             <main>{children}</main>
         </>
     );
